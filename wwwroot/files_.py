07a5c91aa9916946d72f70cfd65b48dd5b641874
Fernando Borovsak <FBOROVSAK@IADB.ORG>
2020-05-15 20:34:31 -0300

    Cambios en listar archivos

--- wwwroot/files_.py
+++ wwwroot/files_.py
@@ -1,20 +1,16 @@
 import uos
 def serve(method):
-    html = """<!DOCTYPE html>
-    <html>
-        <head> <title>ESP8266 Files</title><meta charset="utf-8"></head>
-        <body> <h1>ESP8266 Files</h1>
-            <table border="1"> <tr><th>Archivos</th><th>Peso</th><th colspan="3">Acciones</th></tr> %s </table>
-        </body>
-    </html>
-    """
-    return html % '\n'.join(getRowsDir())
+    if (method == "GET"):
+        return '[%s]' % getDirContent()
 
-def getRowsDir(d=""):
+def getDirContent(d=""):
+    dir = "/" if d == "" else d
+    return '["%s",%d,%s]' % (dir, 1, '[%s]' % ','.join(getDirFiles(d)))
+
+def getDirFiles(d):
     dir = "/" if d == "" else d
     for f in uos.ilistdir(dir):
         if (f[1] == 0x8000):
-            yield '<tr><td>%s</td><td>%d</td><td>📥</td><td>❌</td><td>♻️</td></tr>' % (f[0], f[3])
+            yield '["%s",%d,%s]' % (f[0], 0, f[3])
         else:
-            yield '<tr><td colspan="2">%s</td><td></td><td>❌</td><td>➕</td></tr>' % f[0]
-            yield from getRowsDir(d + "/" + f[0])
+            yield getDirContent(d + "/" + f[0])
